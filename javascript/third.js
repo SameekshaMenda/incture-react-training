e4f38de45d4036f889884a9e26fe29a571bf11d8
CSESCEM <office.cs@sahyadri.edu.in>
2025-07-16 16:57:23 +0530

    day 3

---
+++ javascript/third.js
@@ -0,0 +1,27 @@
+
+
+
+// for(let i=1; i<=1000 ; i++)
+// {
+// let num = i;
+// let flag = true;
+// for(let i =2; i <= num/2 ; i++)
+// {
+//     if(num%i== 0)
+//     {
+//         flag = false;
+//         break;
+//     }
+    
+// }
+
+// if(flag == true)
+// {
+    
+//     console.log(num,"prime");
+// }
+
+
+// }
+
+
