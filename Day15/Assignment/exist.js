9183f185c591ecb3a72d93a2518c36d45d4cf3d3
Sameeksha Menda <141476083+SameekshaMenda@users.noreply.github.com>
2025-07-31 11:53:57 +0530

    Update exist.js

--- Day15/Assignment/exist.js
+++ Day15/Assignment/exist.js
@@ -1,31 +1,33 @@
 function exist(board, word){
-    let row = board.length; //3
-    let col = board[0].length; //4
-    let wordLength = word.length; //6
-    let visted;
-   
-    let directions = [[0,1],[1,0],[0,-1],[-1,0]];
+    let rows = board.length;
+    let cols = board[0].length;
 
-    for(let i = 0; i < row ; i++){
-        for(let j = 0;j < col; j++){
-            if(board[i][j] === word[i]){
-                for(let dir of directions){
-                    let newRow = dir[0]; 
-                    console.log(newRow);
-                    let newCol = dir[1];
-                    console.log(newCol);
-                     visted[newRow][newCol] = true;
-                    if(newRow >= 0 && newRow < row && newCol >= 0 && newCol < col && visted[newRow][newCol] && (board[newRow][newCol] ===word[i])){
-                        
-                       
-                        if(exist(board, word.slice(i+1), visted)){
-                            return true;
-                        }
-                    }
-                }
+    function dfs(row, col, index){
+
+        if(index === word.length) return true;
+
+        if(row < 0 || col < 0 || row >= rows || col >= cols || board[row][col] !== word[index]) return false;
+
+        let temp = board[row][col];
+        board[row][col] = '#'; // mark as visited
+
+        let found = dfs(row + 1, col, index + 1) ||
+                    dfs(row - 1, col , index + 1) ||
+                    dfs(row, col + 1, index + 1) ||
+                    dfs(row , col - 1, index + 1) 
+
+        board[row][col] = temp; // unmark
+        return found;
+    }
+
+    for(let r =0; r < rows; r++)
+    {
+        for(let c = 0; c < cols; c++){
+            if(dfs(r, c, 0)){
+                return true;
             }
         }
-    }return false;
+    }
+    return false
 }
-
-console.log(exist([["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], "ABCCED")); 
+console.log(exist([["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], "ABCCED")); // false
